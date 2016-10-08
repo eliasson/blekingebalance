40e0accd4ff938456c19b10c08154baff9101104
Rickard Jönsson <ric.jonsson@gmail.com>
2016-10-08 14:21:29 +0200

    Validate string before entering loop

--- blekingesaldo/src/main/java/se/fourspaces/blekingebalance/HexUtils.java
+++ blekingesaldo/src/main/java/se/fourspaces/blekingebalance/HexUtils.java
@@ -26,14 +26,19 @@ public abstract class HexUtils {
     public static byte[] hexStringToByteArray(String s) {
         int len = s.length();
         byte[] data = new byte[len / 2];
-        try {
-            for (int i = 0; i < len; i += 2) {
-                data[i / 2] = (byte) ((Character.digit(s.charAt(i), 16) << 4)
-                        + Character.digit(s.charAt(i + 1), 16));
+        String hexStringPattern = "^([0-9A-Fa-f]{2})+$";
+        boolean isHexString = s.matches(hexStringPattern);
+
+        if (isHexString) {
+            try {
+                for (int i = 0; i < len; i += 2) {
+                    data[i / 2] = (byte) ((Character.digit(s.charAt(i), 16) << 4)
+                            + Character.digit(s.charAt(i + 1), 16));
+                }
+            } catch (Exception e) {
+                Log.d(Balance.LOG_TAG, "Argument(s) for hexStringToByteArray(String s)"
+                        + "was not a hex string");
             }
-        } catch (Exception e) {
-            Log.d(Balance.LOG_TAG, "Argument(s) for hexStringToByteArray(String s)"
-                    + "was not a hex string");
         }
         return data;
     }
